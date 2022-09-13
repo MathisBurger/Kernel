eca22d1373b9c075e7eaceb786e86bc162ee69db
Mathis <handymathis@gmail.com>
2022-09-13 19:42:33 +0200

    [TASK] Updated sidebar for relative menu elements

---
+++ Models/Shared/SidebarElement.cs
@@ -0,0 +1,13 @@
+﻿namespace Kernel.Models.Shared;
+
+public class SidebarElement
+{
+    public string Label { get; set; }
+    public string Route { get; set; }
+
+    public SidebarElement(string label, string route)
+    {
+        Label = label;
+        Route = route;
+    }
+}
