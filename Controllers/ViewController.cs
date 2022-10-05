88502e0397bb3ce43ff81383cf98d8cd699b66f9
Mathis <handymathis@gmail.com>
2022-10-05 21:43:40 +0200

    [TASK] Added functional view page

---
+++ Controllers/ViewController.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using Kernel.Filter;
+using Kernel.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kernel.Controllers;
+
+[ApiController]
+//[TypeFilter(typeof(FiltersAuthorization))]
+[Route("api/[controller]")]
+public class ViewController : AuthorizedControllerBase
+{
+    
+    private DbAccess db;
+
+    public ViewController(DbAccess _db)
+    {
+        db = _db;
+    }
+
+    [HttpGet("[action]/{moduleName}/{route}")]
+    //[TypeFilter(typeof(HeadersFilter))]
+    public async Task<ContentResult> View(string moduleName, string route)
+    {
+        var module = await db.ModuleRepository.GetModuleByName(moduleName);
+        if (module == null)
+        {
+            return new ContentResult {
+                ContentType = "text/plain",
+                StatusCode = (int)HttpStatusCode.BadRequest,
+                Content = "Not allowed"
+            };
+        }
+
+        var client = new HttpClient();
+        client.BaseAddress = new Uri(module.ModuleHost!);
+        var resp = await client.GetAsync("/" + route);
+        return new ContentResult {
+            ContentType = "text/html",
+            StatusCode = (int)HttpStatusCode.OK,
+            Content = await resp.Content.ReadAsStringAsync()
+        };
+    }
+    
+}
--- Repository/ModuleRepository.cs
+++ Repository/ModuleRepository.cs
@@ -18,6 +18,11 @@ public class ModuleRepository
         return await Database.Modules.FirstOrDefaultAsync(m => m.ModuleKey == key);
     }
 
+    public async Task<Module?> GetModuleByName(string name)
+    {
+        return await Database.Modules.FirstOrDefaultAsync(m => m.ModuleName == name);
+    }
+
     public async Task<Module[]> FindAll()
     {
         return await Database.Modules.ToArrayAsync();
